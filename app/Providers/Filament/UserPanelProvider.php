2fa7e32e44cd83b4be7767f34adaf789978ee55d
alexmarquezini <alexsmarquez@hotmail.com>
2024-09-13 23:57:52 -0300

    Config Filament Companies and Users Panel

--- app/Providers/Filament/UserPanelProvider.php
+++ app/Providers/Filament/UserPanelProvider.php
@@ -5,7 +5,10 @@ namespace App\Providers\Filament;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Navigation\MenuItem;
+use Filament\Navigation\NavigationItem;
 use Filament\Pages;
+use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
@@ -16,7 +19,10 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Wallo\FilamentCompanies\Pages\User\PersonalAccessTokens;
+use Wallo\FilamentCompanies\Pages\User\Profile;
 
 class UserPanelProvider extends PanelProvider
 {
@@ -33,7 +39,24 @@ class UserPanelProvider extends PanelProvider
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
-                Pages\Dashboard::class,
+                Profile::class,
+                PersonalAccessTokens::class,
+            ])
+            ->userMenuItems([
+                'profile' => MenuItem::make()
+                    ->label('Perfil')
+                    ->icon('heroicon-o-user-circle')
+                    ->url(static fn () => url(Profile::getUrl())),
+                MenuItem::make()
+                    ->label('Empresa')
+                    ->icon('heroicon-o-building-office')
+                    ->url(static fn () => url(Dashboard::getUrl(panel: 'company', tenant: Auth::user()->personalCompany()))),
+            ])
+            ->navigationItems([
+                NavigationItem::make('Tokens de acesso pessoal')
+                    ->label(static fn (): string => __('filament-companies::default.navigation.links.tokens'))
+                    ->icon('heroicon-o-key')
+                    ->url(static fn () => url(PersonalAccessTokens::getUrl())),
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
