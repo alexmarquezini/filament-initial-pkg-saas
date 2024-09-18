37328245e0921f699cee86abbc4d6d0f502d05df
alexmarquezini <alexsmarquez@hotmail.com>
2024-09-18 02:05:40 -0300

    Deploy on vercel with pgslq database on neon

--- config/database.php
+++ config/database.php
@@ -84,17 +84,10 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'url' => env(
+                'DATABASE_URL_UNPOOLED',
+                'postgresql://initial_pkg_saas_owner:endpoint=ep-raspy-firefly-a56wwc7f;lJ7pf6qMbmDQ@ep-raspy-firefly-a56wwc7f.us-east-2.aws.neon.tech/initial_pkg_saas?sslmode=require&charset=utf8'
+            ),
         ],
 
         'sqlsrv' => [
